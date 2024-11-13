792b30a6857ff5d4954430ceff8de7ab51e781fc
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-13 18:49:52 +0400

    feat: hotels page added

---
+++ src/pages/Hotel/data.js
@@ -0,0 +1,93 @@
+import hotel from "./../../assets/images/hotel-resto-4.jpg";
+export const tours = [
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  }
+];
--- src/pages/Hotel/index.jsx
+++ src/pages/Hotel/index.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Hero from "../../components/Hero";
+import ReservationBar from "../../components/ReservationBar";
+import Card from "../../components/Card";
+import { tours } from "./data";
+import './style.scss'
+import AskQuote from "../../components/AskQuote";
 function Hotel() {
-  return <div>Hotel</div>;
+  const buttons = ['<', 1, 2, 3, 4, 5, 6, '>']
+  const [selectedBtn, setselectedBtn] = useState(1);
+  function handleSelect(number) {
+    !isNaN(number) && setselectedBtn(number)
+
+  }
+  return (
+    <div className="Hotel ">
+      <Hero title={"Tours List"} />
+      <div className="container">
+
+        <ReservationBar />
+        <div className="grid">
+          {
+            tours.map(tour => {
+              return <Card key={tour.title} data={tour} />
+
+            })
+          }
+        </div>
+        <div className="btns">
+          {
+            buttons.map(btn => {
+              return <button className={selectedBtn == btn && 'active'} onClick={() => {
+                handleSelect(btn)
+              }} key={btn}>{btn}</button>
+            })
+          }
+        </div>
+        <AskQuote />
+      </div>
+    </div>
+  );
 }
 
 export default Hotel;

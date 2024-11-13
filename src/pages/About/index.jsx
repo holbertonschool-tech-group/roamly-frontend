b24502ba201798cab659b37f6441cdb667127f2f
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-13 18:07:44 +0400

    feat: about page added

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,19 @@
+import AskQuote from "../../components/AskQuote";
+import Feedback from "../../components/Feedback";
 import Hero from "../../components/Hero";
+import Memorable from "../../components/Memorable";
+import Services from "../../components/Services";
+import VideoOverlay from "../../components/VideoOverlay";
 
 function About() {
-  return <div>
+  return <>
     <Hero title={'About Us'} />
-    About</div>;
+    <Services />
+    <VideoOverlay />
+    <Memorable />
+    <Feedback />
+    <AskQuote />
+  </>;
 }
 
 export default About;

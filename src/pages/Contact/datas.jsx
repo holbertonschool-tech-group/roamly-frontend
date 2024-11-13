30b2aaa6f13ab3a1250d37c0571d48e110c498c9
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-13 19:25:04 +0400

    feat: contacts added

---
+++ src/pages/Contact/datas.jsx
@@ -0,0 +1,24 @@
+import { MdLocationOn } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+import { FaPaperPlane } from "react-icons/fa6";
+import { GiEarthAmerica } from "react-icons/gi";
+export const contacts = [
+
+    {
+        title: 'Address',
+        icon: MdLocationOn,
+        info: '198 West 21th Street, Suite 721 New York NY 10016'
+    }, {
+        title: 'Contact Number',
+        icon: FaPhone,
+        info: '+ 1235 2355 98'
+    }, {
+        title: 'Email Address',
+        icon: FaPaperPlane,
+        info: 'info@yoursite.com'
+    }, {
+        title: 'Website',
+        icon: GiEarthAmerica,
+        info: 'yoursite.com'
+    },
+]
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import Hero from "../../components/Hero";
+import AskQuote from "../../components/AskQuote";
+import { contacts } from "./datas";
+import './style.scss'
 function Contact() {
-  return <div>Contact</div>;
+  return <div className="Contact ">
+    <Hero title={"Contact us"} />
+    <div className="container">
+
+      <div className="contacts">
+        {
+          contacts.map(contact => {
+            return <div className="contact" key={contact.title}>
+
+              <div className="icon">
+                {
+                  <contact.icon size={30} color="#ffffff" />
+                }
+              </div>
+              <h1>{contact.title}</h1>
+              <p>{contact.info}</p>
+            </div>
+
+          })
+        }
+      </div>
+    </div>
+    <AskQuote />
+  </div>;
 }
 
 export default Contact;

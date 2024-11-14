4d9bd07bcda9537ca72c6e901faa2a06b221d8e9
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-14 11:57:55 +0400

    fix: card style conflict

--- src/components/Destinations/index.jsx
+++ src/components/Destinations/index.jsx
@@ -1,6 +1,12 @@
-
+import React from "react";
 import './style.scss'
-import { Swiper, SwiperSlide } from 'swiper/react';
+
+import {
+    Swiper,
+    SwiperSlide
+}
+
+from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -9,59 +15,80 @@ import FooterImg from '../../assets/images/footer.png'
 
 
 // import required modules
-import { Pagination } from 'swiper/modules';
-import { cards } from "./card";
+import {
+    Pagination
+}
+
+from 'swiper/modules';
+
+import {
+    cards
+}
+
+from "./card";
+
 function Destinations() {
-    return (
-        <div className="Destinations " style={{ backgroundImage: `url(${FooterImg})` }}>
-            <div className="">
+    return (<div className="Destinations "style= {
+                {
+                backgroundImage: `url(${FooterImg})`
+            }
+        }
 
-                <p>Pacific Provide Places</p>
-                <h1>Select Your Destination</h1>
-                <div className="swiperContainer">
+        > <div className=""> <p>Pacific Provide Places</p> <h1>Select Your Destination</h1> <div className="swiperContainer"> <Swiper slidesPerView= {
+            4
+        }
 
-                    <Swiper
+        spaceBetween= {
+            30
+        }
 
-                        spaceBetween={30}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        modules={[Pagination]}
-                        className="mySwiper"
-                        initialSlide={2}
-                        loop={true}
-                        breakpoints={{
-                            640: {
-                                slidesPerView: 1,
-                                spaceBetween: 10,
-                            },
-                            768: {
-                                slidesPerView: 2,
-                                spaceBetween: 20,
-                            },
-                            1024: {
-                                slidesPerView: 3,
-                                spaceBetween: 30,
-                            },
-                        }}
-                    >
-                        {cards.map(card => {
-                            return <SwiperSlide key={card.title} className="card" style={{ backgroundImage: `url(${card.img})` }}>
-                                <div className="text">
-                                    <h1>{card.title}</h1>
-                                    <span>{card.count} Tours</span>
-                                </div>
+        pagination= {
+                {
+                clickable: true,
+            }
+        }
 
+        modules= {
+            [Pagination]
+        }
 
-                            </SwiperSlide>
-                        })
+        className="mySwiper"
+
+        initialSlide= {
+            2
+        }
+
+        loop= {
+            true
+        }
+
+        > {
+            cards.map(card=> {
+                    return <SwiperSlide key= {
+                        card.title
+                    }
+
+                    className="card"style= {
+                            {
+                            backgroundImage: `url(${card.img})`
                         }
+                    }
 
-                    </Swiper>
-                </div>
-            </div>
-        </div>
-    );
+                    > <div className="texts"> <h1> {
+                        card.title
+                    }
+
+                    </h1> <span> {
+                        card.count
+                    }
+
+                    Tours</span> </div> </SwiperSlide>
+                }
+
+            )
+        }
+
+        </Swiper> </div> </div> </div>);
 }
 
 export default Destinations;

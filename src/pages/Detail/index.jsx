4709aa87a43470866af5eafb1d863b3a886f0f37
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-19 11:20:08 +0400

    style: breakpoints added to detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -23,6 +23,7 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Navigation, Pagination } from "swiper/modules";
+import AskQuote from "../../components/AskQuote";
 
 function Detail() {
     const { id } = useParams();
@@ -30,134 +31,162 @@ function Detail() {
     const data = tours[0];
 
     return (
-        <div className="detailPage container">
-            <Swiper
-                slidesPerView={3}
-                spaceBetween={15}
-                pagination={{
-                    clickable: true
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="imgs"
-                loop={true}
-            >
-                {data.imgs.map((img) => {
-                    return (
-                        <SwiperSlide key={uuidv4()}>
-                            <img src={img} alt="" />
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
-            <div className="head">
-                <h1> {data.title}</h1>
-                <div className="stars">
-                    <div className="icons">
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
-                    </div>
-                    (5.0)
-                </div>
-            </div>
-            <div className="location">
-                <FaLocationDot /> {data.location}
-            </div>
-            <div className="info">
-                <div className="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolore
-                    porro pariatur perspiciatis ullam animi, illo sed fugiat impedit,
-                    minima nulla? Optio pariatur perspiciatis iure quaerat omnis tempore
-                    itaque possimus expedita explicabo blanditiis molestiae tempora ea,
-                    est error natus voluptatibus modi cupiditate accusantium fuga aut
-                    suscipit saepe provident ! Similique optio autem quidem cupiditate
-                    iure obcaecati soluta voluptatem beatae quo quae iusto nisi deleniti,
-                    at ipsam, facere impedit eos nobis accusamus vel fugiat maiores qui.
-                    Quae quidem exercitationem odit maiores quam numquam minus totam
-                    delectus, nostrum odio voluptatibus, laudantium neque in ullam atque
-                    non ex explicabo reprehenderit, quasi voluptatum expedita. Magnam.
-                </div>
-                <div className="book">
-                    <div className="texts">
-                        <h2>Reserve</h2>
-                        <ul>
-                            <li>
-                                <IoIosBed size={20} /> {data.bathrooms}
-                                bathrooms
-                            </li>
-                            <li>
-                                <FaBath size={20} /> {data.bedrooms}
-                                bedrooms
-                            </li>
-                            <li>
-                                <IoCar size={20} /> Free Parking
-                            </li>
-                        </ul>
-                        <div className="price">
-                            <p>total</p> <h4>$ {data.price}</h4>
-                        </div>
-                    </div>
-                    <button>Order</button>
-                </div>
-            </div>
-            <div className="categories">
-                <h3>Categories:</h3>
-                <ul>
-                    {categories.map((category) => {
-                        return (
-                            <li key={uuidv4()}>
-                                <div className="head">
-                                    <p> {category.title}</p> <p> {category.points / 10}</p>
-                                </div>
-                                <div className="line">
-                                    <div
-                                        className="current"
-                                        style={{
-                                            width: `${category.points}%`
-                                        }}
-                                    />
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-            <div className="feedbacks">
-                <h3>Guests who stayed here loved:</h3>
+        <>
+            <div className="detailPage container">
                 <Swiper
-                    slidesPerView={3}
-                    spaceBetween={15}
+
                     pagination={{
                         clickable: true
                     }}
                     navigation={true}
                     modules={[Pagination, Navigation]}
-                    className="reviews"
+                    className="imgs"
                     loop={true}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                    }}
                 >
-                    {data.reviews.map((review) => {
+                    {data.imgs.map((img) => {
                         return (
                             <SwiperSlide key={uuidv4()}>
-                                <div className="card">
-                                    <div className="head">
-                                        <div src="" alt="" className="img">
-                                            {review.name.charAt(0)}
-                                        </div>
-                                        <div className="about">
-                                            <div className="name"> {review.name}</div>
-                                            <div className="country"> {review.country}</div>
-                                        </div>
-                                    </div>
-                                    <div className="content">
-                                        &apos;{review.content}
-                                        &apos;
-                                    </div>
-                                </div>
+                                <img src={img} alt="" />
                             </SwiperSlide>
                         );
                     })}
                 </Swiper>
+                <div className="head">
+                    <h1> {data.title}</h1>
+                    <div className="stars">
+                        <div className="icons">
+                            <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        </div>
+                        (5.0)
+                    </div>
+                </div>
+                <div className="location">
+                    <FaLocationDot /> {data.location}
+                </div>
+                <div className="info">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolore
+                        porro pariatur perspiciatis ullam animi, illo sed fugiat impedit,
+                        minima nulla? Optio pariatur perspiciatis iure quaerat omnis tempore
+                        itaque possimus expedita explicabo blanditiis molestiae tempora ea,
+                        est error natus voluptatibus modi cupiditate accusantium fuga aut
+                        suscipit saepe provident ! Similique optio autem quidem cupiditate
+                        iure obcaecati soluta voluptatem beatae quo quae iusto nisi deleniti,
+                        at ipsam, facere impedit eos nobis accusamus vel fugiat maiores qui.
+                        Quae quidem exercitationem odit maiores quam numquam minus totam
+                        delectus, nostrum odio voluptatibus, laudantium neque in ullam atque
+                        non ex explicabo reprehenderit, quasi voluptatum expedita. Magnam.
+                    </div>
+                    <div className="book">
+                        <div className="texts">
+                            <h2>Reserve</h2>
+                            <ul>
+                                <li>
+                                    <IoIosBed size={20} /> {data.bathrooms}
+                                    bathrooms
+                                </li>
+                                <li>
+                                    <FaBath size={20} /> {data.bedrooms}
+                                    bedrooms
+                                </li>
+                                <li>
+                                    <IoCar size={20} /> Free Parking
+                                </li>
+                            </ul>
+                            <div className="price">
+                                <p>total</p> <h4>$ {data.price}</h4>
+                            </div>
+                        </div>
+                        <button>Order</button>
+                    </div>
+                </div>
+                <div className="categories">
+                    <h3>Categories:</h3>
+                    <ul>
+                        {categories.map((category) => {
+                            return (
+                                <li key={uuidv4()}>
+                                    <div className="head">
+                                        <p> {category.title}</p> <p> {category.points / 10}</p>
+                                    </div>
+                                    <div className="line">
+                                        <div
+                                            className="current"
+                                            style={{
+                                                width: `${category.points}%`
+                                            }}
+                                        />
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="feedbacks">
+                    <h3>Guests who stayed here loved:</h3>
+                    <Swiper
+                        pagination={{
+                            clickable: true
+                        }}
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className="reviews"
+                        loop={true}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                            },
+                            768: {
+                                slidesPerView: 2,
+                                spaceBetween: 20,
+                            },
+                            1024: {
+                                slidesPerView: 3,
+                                spaceBetween: 30,
+                            },
+                        }}
+                    >
+                        {data.reviews.map((review) => {
+                            return (
+                                <SwiperSlide key={uuidv4()}>
+                                    <div className="card">
+                                        <div className="head">
+                                            <div src="" alt="" className="img">
+                                                {review.name.charAt(0)}
+                                            </div>
+                                            <div className="about">
+                                                <div className="name"> {review.name}</div>
+                                                <div className="country"> {review.country}</div>
+                                            </div>
+                                        </div>
+                                        <div className="content">
+                                            &apos;{review.content}
+                                            &apos;
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
+                    </Swiper>
+                </div>
             </div>
-        </div>
+            <AskQuote />
+        </>
     );
 }
 

92eda8d7cec42fbecd9495df0716be0ee8c6c3e7
ferhadesger <ferhadesgerzade14@gmail.com>
2024-11-19 09:28:48 +0400

    feat: hero added to detail page

---
+++ src/pages/Detail/data.js
@@ -0,0 +1,102 @@
+import hotel from "./../../assets/images/hotel-resto-4.jpg";
+export const tours = [
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  },
+  {
+    id: 1,
+    title: "Manila Hotel",
+    price: 200,
+    location: " Banaue, Ifugao, Philippines",
+    bathrooms: 2,
+    bedrooms: 3,
+    img: hotel,
+    duration: 8,
+    near: "Near Mountain"
+  }
+];
--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,12 +1,37 @@
+import { FaStar } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
-
+import { tours } from "./data";
+import './style.scss';
 function Detail() {
     const { id } = useParams();
-
+    // <p>ID: {id}</p>
+    const data = tours[0]
     return (
-        <div>
-            <h1>Detail Page</h1>
-            <p>ID: {id}</p>
+        <div className="detailPage container">
+            <div className="imgs">
+                <img className="img1" src={data.img} alt="" />
+                <img className="img2" src={data.img} alt="" />
+
+            </div>
+            <div className="head">
+                <h1>{data.title}</h1>
+                <div className="stars">
+                    <div className="icons">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                    (5.0)
+                </div>
+            </div>
+            <div className="location">
+                <FaLocationDot />
+                {data.location}
+            </div>
+
         </div>
     );
 }

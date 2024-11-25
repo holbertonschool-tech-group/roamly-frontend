576eb484d8641a98d203fb0af51baea7ae74af82
nihadamirov <nihadamirov98@gmail.com>
2024-11-25 04:27:37 +0400

    update file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-const path = require('path');
-const app = express();
+import path from 'path';
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'dist')));

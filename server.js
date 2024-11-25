17d714bc5402f9a29e79fd119c21b4b7dc170c4d
Nihad Amirov <145747028+nihadamirov@users.noreply.github.com>
2024-11-25 12:52:22 +0400

    Update file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
